00894f10f75d648a9e86595c49f2362599526c37
Kristina <zhozefi@yandex.ru>
2018-11-11 22:09:27 +0300

    add files to client

---
+++ CollOfDishClient/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CollOfDishClient._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+        <p><b>Название статьи:</b> <asp:TextBox ID="TextBox1" runat="server" Height="20px" Width="219px" BorderStyle="Dotted" Font-Size="Large"></asp:TextBox>
+        <asp:Button ID="Button2" runat="server" Text="Редактировать" OnClick="Button2_Click" CssClass="button2" />
+                <br /></p>
+        <asp:Label ID="Label1" runat="server" Text="Статья" CssClass="lable" Font-Size="X-Large"></asp:Label><br />
+        <br />
+    </div>
+
+</asp:Content>
---
+++ CollOfDishClient/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CollOfDishClient.ServiceReference1;
+
+namespace CollOfDishClient
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private class Info //Вложеный класс, который содержит информацию для соединения
+        {
+            public string key;
+            public AgregatorServerSoapClient client;
+            public Info(string k)
+            {
+                key = k;
+            }
+        }
+        protected void Button2_Click(object sender, EventArgs e) //редактировать
+        {
+            Info info = new Info(TextBox1.Text);
+            info.client = new AgregatorServerSoapClient ();
+            string answer;
+            //answer.Code_status = 200;
+            while (true)
+            {
+                if (info.key != null)
+                {
+                    answer = info.client.TestMethod(info.key);
+                    break;
+                }
+                else { Label1.Text = "Укажите название статьи!!"; }
+            }
+            Label1.Text = answer;
+
+        }
+    }
+}
